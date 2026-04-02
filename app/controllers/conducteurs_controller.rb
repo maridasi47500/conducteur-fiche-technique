d966e4ca5bcf2176707d690a9d5540e76d83422a
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2026-04-01 23:20:03 -0300

    qdfg

--- app/controllers/conducteurs_controller.rb
+++ app/controllers/conducteurs_controller.rb
@@ -3,7 +3,7 @@ class ConducteursController < ApplicationController
 
   # GET /conducteurs or /conducteurs.json
   def index
-    @conducteurs = Conducteur.all
+    @conducteurs = Conducteur.all.order(:created_at => :desc)
   end
 
   # GET /conducteurs/1 or /conducteurs/1.json
--- app/models/conducteurline.rb
+++ app/models/conducteurline.rb
@@ -4,4 +4,24 @@ def maduree
 x=read_attribute(:duree)
 ("%02d" % x.hour+":"+ "%02d" % x.min)
 end
+def videoprojection=(uploaded_io)
+
+File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+  file.write(uploaded_io.read)
+end
+write_attribute(:videoprojection, uploaded_io.original_filename)
+end
+def videoprojection
+read_attribute(:videoprojection)
+end
+def son=(uploaded_io)
+
+File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+  file.write(uploaded_io.read)
+end
+write_attribute(:son, uploaded_io.original_filename)
+end
+def son
+read_attribute(:son)
+end
 end
